ed66a2ba08dc18482c272c69e531ccccb4066ac2
meisner87 <87091390+meisner87@users.noreply.github.com>
2022-03-16 19:57:52 +0200

    added buttons to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { IngerdientComponent } from './ingerdient/ingerdient.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    IngerdientComponent,
+    RecipeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
